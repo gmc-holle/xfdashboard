2b5bb65c8e880cfa7fde66556cf9f2086c1dc474
Stephan Haller <nomad@froevel.de>
2013-09-25 06:57:45 +0200

    Added missing files for quicklaunch.

---
+++ src/quicklaunch.c
@@ -0,0 +1,159 @@
+/*
+ * quicklaunch: Quicklaunch box
+ * 
+ * Copyright 2012-2013 Stephan Haller <nomad@froevel.de>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "quicklaunch.h"
+#include "enums.h"
+
+#include <glib/gi18n-lib.h>
+#include <math.h>
+
+/* Define this class in GObject system */
+G_DEFINE_TYPE(XfdashboardQuicklaunch,
+				xfdashboard_quicklaunch,
+				XFDASHBOARD_TYPE_BACKGROUND)
+                                                
+/* Private structure - access only by public API if needed */
+#define XFDASHBOARD_QUICKLAUNCH_GET_PRIVATE(obj) \
+	(G_TYPE_INSTANCE_GET_PRIVATE((obj), XFDASHBOARD_TYPE_QUICKLAUNCH, XfdashboardQuicklaunchPrivate))
+
+struct _XfdashboardQuicklaunchPrivate
+{
+	void *dummy; // TODO: Remove - it's just a dummy
+};
+
+/* Properties */
+enum
+{
+	PROP_LAST
+};
+
+static GParamSpec* XfdashboardQuicklaunchProperties[PROP_LAST]={ 0, };
+
+/* Signals */
+enum
+{
+	SIGNAL_LAST
+};
+
+static guint XfdashboardQuicklaunchSignals[SIGNAL_LAST]={ 0, };
+
+/* Private constants */
+
+/* IMPLEMENTATION: Private variables and methods */
+
+/* IMPLEMENTATION: GObject */
+
+/* Dispose this object */
+static void xfdashboard_quicklaunch_dispose(GObject *inObject)
+{
+	/* Release our allocated variables */
+	XfdashboardQuicklaunchPrivate	*priv=XFDASHBOARD_QUICKLAUNCH(inObject)->priv;
+
+	/* Call parent's class dispose method */
+	G_OBJECT_CLASS(xfdashboard_quicklaunch_parent_class)->dispose(inObject);
+}
+
+/* Set/get properties */
+static void xfdashboard_quicklaunch_set_property(GObject *inObject,
+												guint inPropID,
+												const GValue *inValue,
+												GParamSpec *inSpec)
+{
+	XfdashboardQuicklaunch			*self=XFDASHBOARD_QUICKLAUNCH(inObject);
+
+	switch(inPropID)
+	{
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID(inObject, inPropID, inSpec);
+			break;
+	}
+}
+
+static void xfdashboard_quicklaunch_get_property(GObject *inObject,
+												guint inPropID,
+												GValue *outValue,
+												GParamSpec *inSpec)
+{
+	XfdashboardQuicklaunch			*self=XFDASHBOARD_QUICKLAUNCH(inObject);
+	XfdashboardQuicklaunchPrivate	*priv=self->priv;
+
+	switch(inPropID)
+	{
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID(inObject, inPropID, inSpec);
+			break;
+	}
+}
+
+/* Class initialization
+ * Override functions in parent classes and define properties
+ * and signals
+ */
+static void xfdashboard_quicklaunch_class_init(XfdashboardQuicklaunchClass *klass)
+{
+	GObjectClass		*gobjectClass=G_OBJECT_CLASS(klass);
+
+	/* Override functions */
+	gobjectClass->dispose=xfdashboard_quicklaunch_dispose;
+	gobjectClass->set_property=xfdashboard_quicklaunch_set_property;
+	gobjectClass->get_property=xfdashboard_quicklaunch_get_property;
+
+	/* Set up private structure */
+	g_type_class_add_private(klass, sizeof(XfdashboardQuicklaunchPrivate));
+
+	/* Define properties */
+	// TODO
+
+	// TODO: g_object_class_install_properties(gobjectClass, PROP_LAST, XfdashboardQuicklaunchProperties);
+
+	/* Define signals */
+	// TODO
+}
+
+/* Object initialization
+ * Create private structure and set up default values
+ */
+static void xfdashboard_quicklaunch_init(XfdashboardQuicklaunch *self)
+{
+	XfdashboardQuicklaunchPrivate	*priv;
+
+	priv=self->priv=XFDASHBOARD_QUICKLAUNCH_GET_PRIVATE(self);
+
+	/* This actor is react on events */
+	clutter_actor_set_reactive(CLUTTER_ACTOR(self), TRUE);
+
+	/* Set up default values */
+
+}
+
+/* Implementation: Public API */
+
+/* Create new actor */
+ClutterActor* xfdashboard_quicklaunch_new(void)
+{
+	return(g_object_new(XFDASHBOARD_TYPE_QUICKLAUNCH, NULL));
+}
---
+++ src/quicklaunch.h
@@ -0,0 +1,68 @@
+/*
+ * quicklaunch: Quicklaunch box
+ * 
+ * Copyright 2012-2013 Stephan Haller <nomad@froevel.de>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef __XFOVERVIEW_QUICKLAUNCH__
+#define __XFOVERVIEW_QUICKLAUNCH__
+
+#include "background.h"
+
+G_BEGIN_DECLS
+
+#define XFDASHBOARD_TYPE_QUICKLAUNCH				(xfdashboard_quicklaunch_get_type())
+#define XFDASHBOARD_QUICKLAUNCH(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj), XFDASHBOARD_TYPE_QUICKLAUNCH, XfdashboardQuicklaunch))
+#define XFDASHBOARD_IS_QUICKLAUNCH(obj)				(G_TYPE_CHECK_INSTANCE_TYPE((obj), XFDASHBOARD_TYPE_QUICKLAUNCH))
+#define XFDASHBOARD_QUICKLAUNCH_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass), XFDASHBOARD_TYPE_QUICKLAUNCH, XfdashboardQuicklaunchClass))
+#define XFDASHBOARD_IS_QUICKLAUNCH_CLASS(klass)		(G_TYPE_CHECK_CLASS_TYPE((klass), XFDASHBOARD_TYPE_QUICKLAUNCH))
+#define XFDASHBOARD_QUICKLAUNCH_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS((obj), XFDASHBOARD_TYPE_QUICKLAUNCH, XfdashboardQuicklaunchClass))
+
+typedef struct _XfdashboardQuicklaunch				XfdashboardQuicklaunch;
+typedef struct _XfdashboardQuicklaunchClass			XfdashboardQuicklaunchClass;
+typedef struct _XfdashboardQuicklaunchPrivate		XfdashboardQuicklaunchPrivate;
+
+struct _XfdashboardQuicklaunch
+{
+	/* Parent instance */
+	XfdashboardBackground			parent_instance;
+
+	/* Private structure */
+	XfdashboardQuicklaunchPrivate		*priv;
+};
+
+struct _XfdashboardQuicklaunchClass
+{
+	/*< private >*/
+	/* Parent class */
+	XfdashboardBackgroundClass		parent_class;
+
+	/*< public >*/
+	/* Virtual functions */
+};
+
+/* Public API */
+GType xfdashboard_quicklaunch_get_type(void) G_GNUC_CONST;
+
+ClutterActor* xfdashboard_quicklaunch_new(void);
+
+G_END_DECLS
+
+#endif	/* __XFOVERVIEW_QUICKLAUNCH__ */
