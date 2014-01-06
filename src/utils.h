9f849c753090df9b6eea1310d00a8ebe3e680a7c
Stephan Haller <nomad@froevel.de>
2014-01-06 09:06:38 +0100

    Updating missed files to new year 2014

--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,7 @@
 /*
  * utils: Common functions, helpers and definitions
  * 
- * Copyrigt 2012-2013 Stephan Haller <nomad@froevel.de>
+ * Copyrigt 2012-2014 Stephan Haller <nomad@froevel.de>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
