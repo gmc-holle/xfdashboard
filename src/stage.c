gmc-holle/xfdashboard

commit d82c491d2c15525d4f052100234274936ae03aa5

Do not resize stage after window was opened
This commit addresses issue #1. It seems that starting with clutter 1.16 the way how the stage window is created within clutter changed resulting that coordinates in stage window were transformed wrongly although there were no visible differences. By setting stage actor's size (and therefore window size) when actor is created and before window is created by clutter the right transformation was restored.

src/stage.c
@@ -483,9 +483,6 @@ static void _xfdashboard_stage_on_window_opened(XfdashboardStage *self,
 {
 	XfdashboardStagePrivate				*priv=self->priv;
 	XfdashboardWindowTrackerWindow		*stageWindow;
-	GdkScreen							*screen;
-	gint								primaryMonitor;
-	GdkRectangle						geometry;
 
 	g_return_if_fail(XFDASHBOARD_IS_STAGE(self));
 	g_return_if_fail(XFDASHBOARD_IS_WINDOW_TRACKER_WINDOW(inWindow));
@@ -495,17 +492,6 @@ static void _xfdashboard_stage_on_window_opened(XfdashboardStage *self,
 	/* Check if window opened is this stage window */
 	stageWindow=xfdashboard_window_tracker_window_get_stage_window(CLUTTER_STAGE(self));
 	if(stageWindow!=inWindow) return;
-
-	/* TODO: As long as we do not support multi-monitors
-	 *       use this hack to ensure stage is in right size
-	 */
-	screen=gdk_screen_get_default();
-	primaryMonitor=gdk_screen_get_primary_monitor(screen);
-	gdk_screen_get_monitor_geometry(screen, primaryMonitor, &geometry);
-	clutter_actor_set_size(CLUTTER_ACTOR(self), geometry.width, geometry.height);
-	xfdashboard_window_tracker_window_move_resize(stageWindow,
-													geometry.x, geometry.y,
-													geometry.width, geometry.height);
 
 	/* Set up window for use as stage window */
 	priv->stageWindow=inWindow;
@@ -742,6 +728,9 @@ static void xfdashboard_stage_init(XfdashboardStage *self)
 {
 	XfdashboardStagePrivate		*priv;
 	XfdashboardApplication		*application;
+	GdkScreen					*screen;
+	gint						primaryMonitor;
+	GdkRectangle				geometry;
 
 	priv=self->priv=XFDASHBOARD_STAGE_GET_PRIVATE(self);
 
@@ -769,6 +758,14 @@ static void xfdashboard_stage_init(XfdashboardStage *self)
 	_xfdashboard_stage_setup(self);
 
 	g_signal_connect_swapped(self, "key-release-event", G_CALLBACK(_xfdashboard_stage_on_key_release), self);
+
+	/* TODO: As long as we do not support multi-monitors
+	 *       use this hack to ensure stage is in right size
+	 */
+	screen=gdk_screen_get_default();
+	primaryMonitor=gdk_screen_get_primary_monitor(screen);
+	gdk_screen_get_monitor_geometry(screen, primaryMonitor, &geometry);
+	clutter_actor_set_size(CLUTTER_ACTOR(self), geometry.width, geometry.height);
 
 	/* Connect signal to application */
 	application=xfdashboard_application_get_default();

commit 6db8bb0f708366fedcfe41562916df385f77b643

Fix fullscreen stage window for clutter prior version 1.16
Commit d82c491d2c15525d4f052100234274936ae03aa5 broke correct stage size when window is fullscreen. Add some checks at compile to restore old correct behaviour at least for clutter versions prior the problematic ones (1.16+)

src/stage.c
@@ -483,6 +483,11 @@ static void _xfdashboard_stage_on_window_opened(XfdashboardStage *self,
 {
 	XfdashboardStagePrivate				*priv=self->priv;
 	XfdashboardWindowTrackerWindow		*stageWindow;
+#if !defined(CHECK_CLUTTER_VERSION) || !CLUTTER_CHECK_VERSION(1, 16, 0)
+	GdkScreen							*screen;
+	gint								primaryMonitor;
+	GdkRectangle						geometry;
+#endif
 
 	g_return_if_fail(XFDASHBOARD_IS_STAGE(self));
 	g_return_if_fail(XFDASHBOARD_IS_WINDOW_TRACKER_WINDOW(inWindow));
@@ -496,6 +501,16 @@ static void _xfdashboard_stage_on_window_opened(XfdashboardStage *self,
 	/* Set up window for use as stage window */
 	priv->stageWindow=inWindow;
 	xfdashboard_window_tracker_window_make_stage_window(priv->stageWindow);
+
+#if !defined(CHECK_CLUTTER_VERSION) || !CLUTTER_CHECK_VERSION(1, 16, 0)
+	/* TODO: As long as we do not support multi-monitors
+	 *       use this hack to ensure stage is in right size
+	 */
+	screen=gdk_screen_get_default();
+	primaryMonitor=gdk_screen_get_primary_monitor(screen);
+	gdk_screen_get_monitor_geometry(screen, primaryMonitor, &geometry);
+	clutter_actor_set_size(CLUTTER_ACTOR(self), geometry.width, geometry.height);
+#endif
 
 	/* Disconnect signal handler as this is a one-time setup of stage window */
 	g_debug("Stage window was opened and set up. Removing signal handler.");
@@ -728,9 +743,11 @@ static void xfdashboard_stage_init(XfdashboardStage *self)
 {
 	XfdashboardStagePrivate		*priv;
 	XfdashboardApplication		*application;
+#if defined(CLUTTER_CHECK_VERSION) && CLUTTER_CHECK_VERSION(1, 16, 0)
 	GdkScreen					*screen;
 	gint						primaryMonitor;
 	GdkRectangle				geometry;
+#endif
 
 	priv=self->priv=XFDASHBOARD_STAGE_GET_PRIVATE(self);
 
@@ -759,6 +776,7 @@ static void xfdashboard_stage_init(XfdashboardStage *self)
 
 	g_signal_connect_swapped(self, "key-release-event", G_CALLBACK(_xfdashboard_stage_on_key_release), self);
 
+#if defined(CLUTTER_CHECK_VERSION) && CLUTTER_CHECK_VERSION(1, 16, 0)
 	/* TODO: As long as we do not support multi-monitors
 	 *       use this hack to ensure stage is in right size
 	 */
@@ -766,6 +784,7 @@ static void xfdashboard_stage_init(XfdashboardStage *self)
 	primaryMonitor=gdk_screen_get_primary_monitor(screen);
 	gdk_screen_get_monitor_geometry(screen, primaryMonitor, &geometry);
 	clutter_actor_set_size(CLUTTER_ACTOR(self), geometry.width, geometry.height);
+#endif
 
 	/* Connect signal to application */
 	application=xfdashboard_application_get_default();