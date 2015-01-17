9c4177e72512511f32629c31681f557d5c966ddb
Stephan Haller <nomad@froevel.de>
2015-01-17 19:29:17 +0100

    Fixed years in copyright message

--- xfdashboard/utils.c
+++ xfdashboard/utils.c
@@ -1,7 +1,7 @@
 /*
  * utils: Common functions, helpers and definitions
  * 
- * Copyright 2012 Stephan Haller <nomad@froevel.de>
+ * Copyright 2012-2015 Stephan Haller <nomad@froevel.de>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
