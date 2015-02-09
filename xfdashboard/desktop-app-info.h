180b7b90df14eeb6eb6ea47c85ef8828f0db3d72
Stephan Haller <nomad@froevel.de>
2015-02-09 20:04:46 +0100

    Impleting XfdashboardDesktopAppInfo
    XfdashboardDesktopAppInfo is a new class wrapping a GarconMenuItem and a string for the desktop ID and it also implements the GAppInfo interface. So it can be used - more or less - as a drop-in replacement for GDesktopAppInfo. Each function of GAppInfo interface is implemented except for function regarding to mime type associations. This class wraps GarconMenuItem to gain all it's advantages over GDesktopAppInfo, e.g. supporting desktop file of type "Link" to start a web-browser with the url specified in desktop file. It also forwards the "changed" signal of GarconMenuItem (if it did set up a file monitor for it) to notify about changes to the menu item and the desktop file. So an actor could adjust its appearing to the changes made.
    
    This class also has an advance lookup function for desktop IDs containing dashes. It should find a desktop file for nearly any possible combination of a dash being a directory separator or being part of the file/directory name.
    
    This commit addresses isse #67

---
+++ xfdashboard/desktop-app-info.h
@@ -0,0 +1,79 @@
+/*
+ * desktop-app-info: A GDesktopAppInfo like object for garcon menu
+ *                   items implementing and supporting GAppInfo
+ * 
+ * Copyright 2012-2015 Stephan Haller <nomad@froevel.de>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef __XFDASHBOARD_DESKTOP_APP_INFO__
+#define __XFDASHBOARD_DESKTOP_APP_INFO__
+
+#include <garcon/garcon.h>
+
+G_BEGIN_DECLS
+
+#define XFDASHBOARD_TYPE_DESKTOP_APP_INFO				(xfdashboard_desktop_app_info_get_type())
+#define XFDASHBOARD_DESKTOP_APP_INFO(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj), XFDASHBOARD_TYPE_DESKTOP_APP_INFO, XfdashboardDesktopAppInfo))
+#define XFDASHBOARD_IS_DESKTOP_APP_INFO(obj)			(G_TYPE_CHECK_INSTANCE_TYPE((obj), XFDASHBOARD_TYPE_DESKTOP_APP_INFO))
+#define XFDASHBOARD_DESKTOP_APP_INFO_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass), XFDASHBOARD_TYPE_DESKTOP_APP_INFO, XfdashboardDesktopAppInfoClass))
+#define XFDASHBOARD_IS_DESKTOP_APP_INFO_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE((klass), XFDASHBOARD_TYPE_DESKTOP_APP_INFO))
+#define XFDASHBOARD_DESKTOP_APP_INFO_GET_CLASS(obj)		(G_TYPE_INSTANCE_GET_CLASS((obj), XFDASHBOARD_TYPE_DESKTOP_APP_INFO, XfdashboardDesktopAppInfoClass))
+
+typedef struct _XfdashboardDesktopAppInfo				XfdashboardDesktopAppInfo;
+typedef struct _XfdashboardDesktopAppInfoClass			XfdashboardDesktopAppInfoClass;
+typedef struct _XfdashboardDesktopAppInfoPrivate		XfdashboardDesktopAppInfoPrivate;
+
+struct _XfdashboardDesktopAppInfo
+{
+	/* Parent instance */
+	GObject								parent_instance;
+
+	/* Private structure */
+	XfdashboardDesktopAppInfoPrivate	*priv;
+};
+
+struct _XfdashboardDesktopAppInfoClass
+{
+	/*< private >*/
+	/* Parent class */
+	GObjectClass						parent_class;
+
+	/*< public >*/
+	/* Virtual functions */
+	void (*changed)(XfdashboardDesktopAppInfo *self);
+};
+
+/* Public API */
+GType xfdashboard_desktop_app_info_get_type(void) G_GNUC_CONST;
+
+GAppInfo* xfdashboard_desktop_app_info_new_from_desktop_id(const gchar *inDesktopID);
+GAppInfo* xfdashboard_desktop_app_info_new_from_path(const gchar *inPath);
+GAppInfo* xfdashboard_desktop_app_info_new_from_file(GFile *inFile);
+GAppInfo* xfdashboard_desktop_app_info_new_from_menu_item(GarconMenuItem *inMenuItem);
+
+gboolean xfdashboard_desktop_app_info_is_valid(XfdashboardDesktopAppInfo *self);
+
+GFile* xfdashboard_desktop_app_info_get_file(XfdashboardDesktopAppInfo *self);
+
+gboolean xfdashboard_desktop_app_info_reload(XfdashboardDesktopAppInfo *self);
+
+G_END_DECLS
+
+#endif	/* __XFDASHBOARD_DESKTOP_APP_INFO__ */
