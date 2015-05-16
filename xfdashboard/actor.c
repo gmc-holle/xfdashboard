ff26e5870f0ceaa9417eafb981c455b3e82ba217
Stephan Haller <nomad@froevel.de>
2015-05-16 19:28:50 +0200

    Free effect list after duplication at XfdashboardActor

--- xfdashboard/actor.c
+++ xfdashboard/actor.c
@@ -257,6 +257,7 @@ static void _xfdashboard_actor_update_effects(XfdashboardActor *self, const gcha
 	priv->effects=g_strdup(effectsList);
 
 	/* Release allocated resources */
+	if(effectsList) g_free(effectsList);
 	if(effectIDs) g_strfreev(effectIDs);
 	g_object_unref(themeEffects);
 }
