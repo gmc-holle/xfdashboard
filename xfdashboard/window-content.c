a59674806904c1794b5d349a883cec2707c9a417
Stephan Haller <nomad@froevel.de>
2015-05-16 19:51:33 +0200

    Free ClutterColor when object instance XfdashboardWindowContent is disposed

--- xfdashboard/window-content.c
+++ xfdashboard/window-content.c
@@ -1423,6 +1423,12 @@ static void _xfdashboard_window_content_dispose(GObject *inObject)
 		priv->suspendSignalID=0;
 	}
 
+	if(priv->outlineColor)
+	{
+		clutter_color_free(priv->outlineColor);
+		priv->outlineColor=NULL;
+	}
+
 	if(priv->styleClasses)
 	{
 		g_free(priv->styleClasses);
