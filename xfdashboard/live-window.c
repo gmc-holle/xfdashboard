2a0fb47a2dc017e218b3931a9b1c5f332cc26ba0
Stephan Haller <nomad@froevel.de>
2016-02-10 09:11:28 +0100

    Add class '.window-state-maximized' to actors of type XfdashboardLiveWindow when the associated window is maximized.
    That's just a new class added to the already present classes '.window-state-pinned', '.window-state-minimized' and '.window-state-urgent'.

--- xfdashboard/live-window.c
+++ xfdashboard/live-window.c
@@ -294,6 +294,16 @@ static void _xfdashboard_live_window_on_state_changed(XfdashboardLiveWindow *sel
 			xfdashboard_stylable_remove_class(XFDASHBOARD_STYLABLE(self), "window-state-minimized");
 		}
 
+	/* Add or remove class depending on 'maximized' window state */
+	if(xfdashboard_window_tracker_window_is_maximized(inWindow))
+	{
+		xfdashboard_stylable_add_class(XFDASHBOARD_STYLABLE(self), "window-state-maximized");
+	}
+		else
+		{
+			xfdashboard_stylable_remove_class(XFDASHBOARD_STYLABLE(self), "window-state-maximized");
+		}
+
 	/* Add or remove class depending on 'urgent' window state */
 	if(xfdashboard_window_tracker_window_is_urgent(inWindow))
 	{
